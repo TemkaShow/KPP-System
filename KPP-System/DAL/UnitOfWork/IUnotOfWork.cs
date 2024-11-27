5442480882dfdab1d33d7ed502714afbd9ac38ba
P3nk4 <83232977+TemkaShow@users.noreply.github.com>
2024-11-27 15:57:12 +0200

    UnitOfWork

---
+++ KPP-System/DAL/UnitOfWork/IUnotOfWork.cs
@@ -0,0 +1,17 @@
+﻿using DAL.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.UnitOfWork
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IUserRepository Users { get; }
+        IZoneRepository Zones { get; }
+        IPassRepository Passes { get; }
+        void Save();
+    }
+}
