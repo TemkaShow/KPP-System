a26a7fea6d481387800289beedd998a639de23b5
P3nk4 <83232977+TemkaShow@users.noreply.github.com>
2024-11-27 15:54:03 +0200

    Create entities

---
+++ KPP-System/DAL/Entities/Pass.cs
@@ -0,0 +1,17 @@
+﻿using DAL.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Entities
+{
+    public class Pass
+    {
+        public int Id { get; set; }
+        public DateTime IssueDate { get; set; }
+        public DateTime ExpiretionDate { get; set; }
+        public PassStatus Status { get; set; }
+    }
+}
