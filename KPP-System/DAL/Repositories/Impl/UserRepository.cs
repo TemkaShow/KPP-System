fc419117931512273fe8baed2e72316d6b24f22b
P3nk4 <83232977+TemkaShow@users.noreply.github.com>
2024-11-27 15:56:31 +0200

    Repos, EF

---
+++ KPP-System/DAL/Repositories/Impl/UserRepository.cs
@@ -0,0 +1,16 @@
+﻿using DAL.EF;
+using DAL.Entities;
+using DAL.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repositories.Impl
+{
+    public class UserRepository : BaseRepository<User>, IUserRepository
+    {
+        internal UserRepository(KPPContext context) : base(context) { }
+    }
+}
