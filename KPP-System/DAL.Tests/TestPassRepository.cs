7e916fbcb0484f5d3c7ab56c810164c84247a630
P3nk4 <83232977+TemkaShow@users.noreply.github.com>
2024-12-12 15:12:32 +0200

    Added DAL.Tests proj & TestPassRepository

---
+++ KPP-System/DAL.Tests/TestPassRepository.cs
@@ -0,0 +1,16 @@
+﻿using DAL.Entities;
+using DAL.Repositories.Impl;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Tests
+{
+    class TestPassRepository : BaseRepository<Pass>
+    {
+        public TestPassRepository(DbContext context) : base(context) { }
+    }
+}
